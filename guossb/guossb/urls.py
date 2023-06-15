bc1dec0d78445e5b751d1199c0d7d2287031374f
星言 <75352067+2306345203YH@users.noreply.github.com>
2023-06-15 14:56:14 +0800

    Add files via upload

---
+++ guossb/guossb/urls.py
@@ -0,0 +1,25 @@
+"""guossb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import uniappclient.views as uniapp
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('index/',uniapp.index),
+    path('getches/',uniapp.getces),
+    path('duotp/',uniapp.duoiphone),
+    path('onetp/',uniapp.oneiphone)
+]
